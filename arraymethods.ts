8d4ef23794f8b59f390a572a06b8adfbdce4e41c
nagehancapa <capanagehan@gmail.com>
2022-02-14 12:33:32 +0100

    added objects arrays practices

---
+++ arraymethods.ts
@@ -0,0 +1,10 @@
+var levels: number[] = [20, 30, 12, 30, 100, 20];
+console.log(levels.toString());
+console.log(levels.join(" "));
+console.log(levels.slice(3, 5));
+levels.splice(2, 3, 88, 99);
+console.log(levels.toString());
+levels.push(12, 20, 30);
+console.log(levels.toString());
+console.log(levels.pop());
+console.log(levels.toString());
